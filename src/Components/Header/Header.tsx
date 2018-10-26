c67266d048b1fd412a1805f7d78fbc7bf2728578
Grzegorz Martowski <grzegorz.martowski@avra.pl>
2018-10-26 12:51:13 +0200

    refaktoryzacja grafik

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Provider, AppContext } from '../../App/Provider';
 import i18next from "i18next";
 import { translate } from "react-i18next";
 import { Navbar } from "../Navbar/Navbar";
@@ -15,23 +16,33 @@ export class Header extends Component {
     public render() {
         const {t} = this.props;
         return (
-            <header className="header">
-                <div className="header__logo">
-                    <Link to="/">
-                        <Image src="http://stabsoft.pl/public/images/brands/stabsoft.png"
-                               alt="Logo Stabsoft"
-                               title="Logo Stabsoft"
-                        />
-                    </Link>
-                </div>
-                <div className="header__navbar">
-                    <Navbar />
-                </div>
-                <div className="header__locale">
-                    <Button type="positive" onClickHandler={this.onChangeLanguage}>{t("header.langPl")}</Button>
-                    <Button type="positive" onClickHandler={this.onChangeLanguage}>{t("header.langEn")}</Button>
-                </div>
-            </header>
+            <Provider>
+                <AppContext.Consumer>
+                    {
+                        (context) => (
+                            <header className="header">
+                                <div className="header__logo">
+                                    <Link to="/">
+                                        <Image src={context.logo}
+                                               alt="Logo Stabsoft"
+                                               title="Logo Stabsoft"
+                                        />
+                                    </Link>
+                                </div>
+                                <div className="header__navbar">
+                                    <Navbar />
+                                </div>
+                                <div className="header__locale">
+                                    <Button type="positive"
+                                            onClickHandler={this.onChangeLanguage}>{t("header.langPl")}</Button>
+                                    <Button type="positive"
+                                            onClickHandler={this.onChangeLanguage}>{t("header.langEn")}</Button>
+                                </div>
+                            </header>
+                        )
+                    }
+                </AppContext.Consumer>
+            </Provider>
         );
     }
 }
--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import { toolsFromPast, toolsCurrent, toolsWishList } from '../../experience-items.json';
 import { translate } from "react-i18next";
 import { Image } from "../../Components/Image/Image";
 import { Provider, AppContext } from '../../App/Provider';
 import './about.less';
+import { Title } from '../../Components/Title/Title';
+
+interface IAboutProps {
+    t: void
+}
 
 @translate("common")
-export class About extends Component {
+export class About extends Component<IAboutProps> {
 
     public render() {
 
@@ -22,82 +28,42 @@ export class About extends Component {
                                        title={context.ceo}
                                 />
                                 <div className="about__content">
-                                    {t("about.content")}
+
+                                    <div className="about__text">
+                                        <p className="cite">{t("about.experience")}</p>
+                                        <p className="cite">{t("about.codingIsFun")}</p>
+                                        <p className="cite">{t("about.interestedIn")}</p>
+                                    </div>
+
                                     <div className="about__tools">
 
                                         <div className="about__tools-used">
-                                            <h2>Technologie z którymi pracowałem: </h2>
-                                            <Image src={require("../../images/angular.png")}
-                                                   title="AngularJS"
-                                                   alt="AngularJS" />
-                                            <Image src={require("../../images/jquery.png")}
-                                                   title="jQuery"
-                                                   alt="jQuery" />
-                                            <Image src={require("../../images/wordpress.png")}
-                                                   title="Wordpress"
-                                                   alt="Wordpress" />
-
-                                            <Image src={require("../../images/grunt.png")}
-                                                   title="Grunt"
-                                                   alt="Grunt" />
-
-                                            <Image src={require("../../images/gulp.png")}
-                                                   title="Gulp"
-                                                   alt="Gulp" />
+                                            <Title markup={'h2'} title={t('about.pastToolsTitle')} />
+                                            {
+                                                Object.keys(toolsFromPast).map((tool) => (
+                                                    <Image src={toolsFromPast[tool]}
+                                                           title={tool}
+                                                           alt={tool} />
+                                                ))
+                                            }
                                         </div>
 
                                         <div className="about__tools-current">
-                                            <h2>Technologie jakich teraz używam: </h2>
-                                            <Image src={require("../../images/js.png")}
-                                                   title="Javascript"
-                                                   alt="Javascript" />
-
-                                            <Image src={require("../../images/html5.png")}
-                                                   title="Html5"
-                                                   alt="Html5" />
-
-                                            <Image src={require("../../images/css3.png")}
-                                                   title="CSS3"
-                                                   alt="CSS3" />
-
-                                            <Image src={require("../../images/reactjs.png")}
-                                                   title="ReactJS"
-                                                   alt="ReactJS" />
-
-                                            <Image src={require("../../images/webpack.png")}
-                                                   title="Webpack"
-                                                   alt="Webpack" />
-
-                                            <Image src={require("../../images/yarn.png")}
-                                                   title="Yarn"
-                                                   alt="Yarn" />
-
-                                            <Image src={require("../../images/jest.png")}
-                                                   title="Jest"
-                                                   alt="Jest" />
-
-
-                                            <Image src={require("../../images/enzyme.png")}
-                                                   title="Enzyme"
-                                                   alt="Enzyme" />
-
-                                            <Image src={require("../../images/scrum.png")}
-                                                   title="Scrum"
-                                                   alt="Scrum" />
+                                            <Title markup={'h2'} title={t('about.presentToolsTitle')} />
+                                            {
+                                                Object.keys(toolsCurrent).map((tool) => (
+                                                    <Image src={toolsCurrent[tool]} alt={tool} title={tool} />
+                                                ))
+                                            }
                                         </div>
+
                                         <div className="about__tools-learned">
-                                            <h2>Czego się obecnie uczę:</h2>
-                                            <Image src={require("../../images/nodejs.png")}
-                                                   title="Cypress"
-                                                   alt="Cypress" />
-
-                                            <Image src={require("../../images/cypress.png")}
-                                                   title="Cypress"
-                                                   alt="Cypress" />
-
-                                            <Image src={require("../../images/mongodb.png")}
-                                                   title="Mongo DB"
-                                                   alt="Mongo DB" />
+                                            <Title markup={'h2'} title={t('about.futureToolsTitle')} />
+                                            {
+                                                Object.keys(toolsWishList).map((tool) => (
+                                                    <Image src={toolsWishList[tool]} alt={tool} title={tool} />
+                                                ))
+                                            }
                                         </div>
                                     </div>
                                 </div>
