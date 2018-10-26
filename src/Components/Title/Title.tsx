0f129371e7bf2bd82a261300669a68780e1fcbaf
Grzegorz Martowski <grzegorz.martowski@avra.pl>
2018-10-26 15:33:27 +0200

    refactoring components, adding favicon

--- src/Components/Title/Title.tsx
+++ src/Components/Title/Title.tsx
@@ -1,30 +1,23 @@
-import React, { Component, Fragment } from 'react';
+import React from 'react';
 
-interface IPropsTitle {
+interface ITitleProps {
     markup: string,
     title: string
 }
 
-export class Title extends Component<IPropsTitle> {
-
-    private getTitleTag = (markup: string) => {
-
-        const {title} = this.props;
-
-        if (markup == 'h1') {
-            return <h1>{title}</h1>;
-        } else if (markup == 'h2') {
-            return <h2>{title}</h2>;
-        } else if (markup == 'h3') {
-            return <h3>{title}</h3>;
-        } else {
-            return <span>{title}</span>;
-        }
-    };
-
-    public render() {
-        return (
-            <Fragment>{this.getTitleTag(this.props.markup)}</Fragment>
-        );
+function getTitleTag(markup: string, title: string) {
+    if (markup == 'h1') {
+        return <h1>{title}</h1>;
+    } else if (markup == 'h2') {
+        return <h2>{title}</h2>;
+    } else if (markup == 'h3') {
+        return <h3>{title}</h3>;
+    } else {
+        return <span>{title}</span>;
     }
 }
+
+export const Title: React.SFC<ITitleProps> = props => {
+    const {markup, title} = props;
+    return getTitleTag(markup, title);
+};
---
+++ src/Components/Title/title.spec.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// given
+describe('Title Component', () => {
+
+    const {component} = render();
+
+    // then
+    it('should render title with proper markup', () => {
+
+    });
+});
+
+function render() {
+
+
+    return {component};
+}
